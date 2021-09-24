0043a3c5be1b74f9257d989578144b5bee34c079
rockykev <kev.rocky@gmail.com>
2021-09-24 14:29:37 -0700

    another one

--- gh-pages-deploy.js
+++ gh-pages-deploy.js
@@ -19,14 +19,16 @@ const rmrf = require("rimraf");
     await execa("git", ["--work-tree", folderName, "commit", "-m", "gh-pages"]);
     console.log("Pushing to gh-pages...");
     await execa("git", ["push", "origin", "HEAD:gh-pages", "--force"]);
-    await rmrf(folderName, { glob: false });
+    await rmrf(folderName, { glob: false }, () => {
+      console.log("Deleted build folder" + folderName)
+    });
     console.log("Successfully deployed");
   } catch (e) {
     // eslint-disable-next-line no-console
     console.log(e.message);
     exitCode = 1;
   } finally {
-    await fs.promises.writeFile(configFilePath, originPublicPath, fileOpts);
+    // await fs.promises.writeFile(configFilePath, originPublicPath, fileOpts);
     await execa("git", ["checkout", "-f", "master"]);
     await execa("git", ["branch", "-D", "gh-pages"]);
     console.log("Successfully deployed, check your settings");
